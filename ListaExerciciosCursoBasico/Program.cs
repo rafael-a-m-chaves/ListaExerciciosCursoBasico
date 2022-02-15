3257e3e75f488ad2acb2dcce10261805d1df54fa
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-15 11:12:59 -0300

    final lista com enunciados e codigo mais limpo

--- ListaExerciciosCursoBasico/Program.cs
+++ ListaExerciciosCursoBasico/Program.cs
@@ -9,7 +9,6 @@ namespace Exercicio_1
         {
             //Escreva um programa que exiba os números de 1 a 1000 na tela em ordem decrescente
 
-
             for (var i = 1000; i >= 1; i--) Console.WriteLine(i); // Quando o laço ou o condicional for de uma linha pode ser feito desse jeito
 
             Console.ReadKey();
